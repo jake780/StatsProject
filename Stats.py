2b318e804328e24f7c84e4dd67b954b35ae8369d
jake780 <byebyewrxsti@gmail.com>
2023-02-02 17:05:47 -0700

    updated

--- Stats.py
+++ Stats.py
@@ -1,9 +1,14 @@
+from random import choice
+
 class TempStats():
     """Loads and Analyzes average temperature data"""
     def __init__(self):
         self.tempDict = {}
         self.sortedTemps = []
         self.loadData()
+        self.n = len(self.tempDict)
+        self.mean = self.sampleMean()
+
 
     def loadData(self):
         """Load the temperature data into tempDict from the csv file"""
@@ -18,9 +23,9 @@ class TempStats():
 
     def __str__(self):
         """Displays a table of the average temperature data"""
-        formattedStr = f"\nDate     : Average Temp(F)\n"
+        formattedStr = f"\nDate      : Average Temp(F)\n"
         for item in self.tempDict:
-            formattedStr += f"{item} : {self.tempDict[item]}\n"
+            formattedStr += f"{item}{' '*(9-len(item))} : {self.tempDict[item]}\n"
         return formattedStr
 
     def analyzeData(self):
@@ -32,30 +37,70 @@ class TempStats():
         # Sample Median
         print(f"Sample Median: {self.sampleMedian()}\n")
 
+        # Sample Variance
+        print(f"Sample Variance: {'%.2f' % self.sampleVariance()}\n")
+
+        # Sample Standard Deviation
+        print(f"Sample Standard Dev: {'%.2f' % self.sampleStandardDeviation()}\n")
+
+        # Simple Random Sample of 20
+        print("Simple Random Sample of size 20\n")
+        sample = self.randomSample(20)
+        print(f"Sample Mean: {sample[0]}\nSample Variance: {sample[1]}")
+
     def sampleMean(self):
         """Returns the average temperature"""
         sum = 0
         for item in self.tempDict:
             sum += self.tempDict[item]
-        return (sum/len(self.tempDict))
+        return (sum/self.n)
 
     def sampleMedian(self):
         """Returns the median temperature"""
-        n = len(self.sortedTemps)
-        if  (n % 2) == 0:
+        if  (self.n % 2) == 0:
             #Even
-            nLeft = int(n/2)
-            nRight = int((n/2)+1)
+            nLeft = int(self.n/2)
+            nRight = int((self.n/2)+1)
             return (self.sortedTemps[nLeft] + self.sortedTemps[nRight])/2
         else:
             #Odd
-            return self.sortedTemps[n]
-            
+            return self.sortedTemps[self.n]
+
+    def sampleVariance(self):
+        """Returns the sample variance of the data"""
+        sum = 0
+        for item in self.sortedTemps:
+            sum += ((item - self.mean)**2)
+        return sum/self.n
+
+    def sampleStandardDeviation(self):
+        """Returns the sample Standard Deviation"""
+        return self.sampleVariance()**0.5
+
+    def randomSample(self, amount):
+        """Randomly chooses amount items out of the population and computes the mean and sample variance"""
+        samples = []
+        for _ in range(amount):
+            samples.append(choice(self.sortedTemps))
+
+        # Sample mean
+        sum = 0
+        for s in samples:
+            sum += s
+        mean = sum/amount
+
+        # Sample Variance
+        sum = 0
+        for s in samples:
+            sum += ((s-mean)**2)
+        variance = sum/amount
+
+        return mean, variance
+        
 
 def main():
     T = TempStats()
-
-    print(T)
+    #print(T)
 
     T.analyzeData()
 
